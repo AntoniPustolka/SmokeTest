7e4b8aab21a90757fce245d37ff92162511f5790
student <tester@tester-m>
2020-04-26 11:59:34 +0000

    Testing on www.sukienkimm.pl

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
+from time import sleep
 import unittest
 from selenium import webdriver
 
-class MojPrzypadekTestow(unittest.TestCase):
-
+class MojPrzypadekTestowy(unittest.TestCase):
     def setUp(self):
-        print("przygotowanie do testu")
-        self.driver=webdriver.Firefox()
+        print("Przygotowanie testu")
+        self.driver = webdriver.Firefox()
+        self.driver.get("https://www.sukienkimm.pl/")
+        self.driver.maximize_window()
 
     def testSelenium(self):
-        print('Test')
+        driver = self.driver
+        print("Wylaczam Alert")
+        al=driver.find_element_by_id('onesignal-popover-cancle-button')
+        al.click()
+        print("Klikam zaloguj sie")
+        zaloguj_btn = driver.find_element_by_xpath('//span[text()=" zaloguj się"]')
+        zaloguj_btn.click()
+        # Sleep tylko w celach debugowych
+        sleep(2)
 
     def tearDown(self):
-        print('Sprzatanie po tescie!')
+        print("sprzatanie po tescie")
+        self.driver.quit()
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
